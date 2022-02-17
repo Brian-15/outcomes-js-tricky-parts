bd34a4168b0bf4a48ec1881a825b77611ff5321a
Brian-15 <bcirieco@gmail.com>
2022-02-17 14:06:19 -0800

    implemented curriedAdd

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,11 @@
 function curriedAdd(total) {
-
+  let sum = total ? total : 0;
+  if (Number.isInteger(total)) return function(add) {
+    if (!add) return sum;
+    sum += add;
+    return curriedAdd(sum);
+  }
+  return sum;
 }
 
 module.exports = { curriedAdd };
