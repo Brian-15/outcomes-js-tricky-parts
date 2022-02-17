c06a06a08c30ea2e472090d37cf37c4d3c750743
Brian-15 <bcirieco@gmail.com>
2022-02-16 17:49:24 -0800

    implemented guessingGame

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,18 @@
 function guessingGame() {
-
+  const randNum = Math.floor(Math.random() * 100);
+  let numGuesses = 0, hasWon = false;;
+  return function(guess) {
+    if (hasWon) return "The game is over, you already won!";
+    numGuesses++;
+    if (guess < randNum) {
+      return `${guess} is too low!`;
+    } else if (guess > randNum) {
+      return `${guess} is too high!`;
+    } else {
+      hasWon = true;
+      return `You win! You found ${guess} in ${numGuesses} guesses.`;
+    }
+  };
 }
 
 module.exports = { guessingGame };
