8c34a75b352d8a200ad667151273e71d1d4244ad
Brian-15 <bcirieco@gmail.com>
2022-02-16 18:28:38 -0800

    implemented createAccount

--- create-account.js
+++ create-account.js
@@ -1,5 +1,28 @@
 function createAccount(pin, amount) {
-
+  let userPin = pin;
+  let balance = amount ? amount : 0;
+  return {
+    checkBalance: function(enteredPin) {
+      if (userPin !== enteredPin) return "Invalid PIN.";
+      return "$" + balance;
+    },
+    deposit: function(enteredPin, depositAmount) {
+      if (userPin !== enteredPin) return "Invalid PIN.";
+      balance += depositAmount;
+      return `Successfully deposited $${depositAmount}. Current balance: $${balance}.`;
+    },
+    withdraw: function(enteredPin, withdrawAmount) {
+      if (userPin !== enteredPin) return "Invalid PIN.";
+      if (balance < withdrawAmount) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      balance -= withdrawAmount;
+      return `Successfully withdrew $${withdrawAmount}. Current balance: $${balance}.`;
+    },
+    changePin: function(oldPin, newPin) {
+      if (oldPin !== userPin) return "Invalid PIN.";
+      userPin = newPin;
+      return "PIN successfully changed!";
+    }
+  };
 }
 
 module.exports = { createAccount };
